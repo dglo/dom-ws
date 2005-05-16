e92a284da558ddc36d90227991edf8544d49cf47
Arthur Jones <arthur@lbl.gov>
2005-05-16 06:55:05 -0500

    no longer need tarballs on my home dirctory on glacier

--- cpprod.sh
+++ cpprod.sh
@@ -13,19 +13,13 @@ num=`cat prod.num`
 #
 # cp files and setup new link...
 #
-scp prod-${rev}-${num}.tar.gz getprod.sh glacier.lbl.gov:public_html/prod
 scp prod-${rev}-${num}.tar.gz \
   glacier.lbl.gov:/home/icecube/system/httpd/htdocs/releases/DOM-MB/stable_hex/
-ssh glacier.lbl.gov \
- "cd public_html/prod; rm -f prod-latest.tar.gz;\
- ln -s prod-${rev}-${num}.tar.gz prod-latest.tar.gz"
 
 ssh glacier.lbl.gov \
  "cd /home/icecube/system/httpd/htdocs/releases/DOM-MB/stable_hex; \
   rm -f prod-latest.tar.gz; \
   ln -s prod-${rev}-${num}.tar.gz prod-latest.tar.gz"
-
-scp ChangeLog glacier.lbl.gov:/home/icecube/system/httpd/htdocs/releases/DOM-MB/stable_hex
 
 exec ./tagprod.sh
 
